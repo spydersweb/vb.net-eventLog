c304d8801e0239d648a12b9b58da74aa5ba07605
Graham Cole <52936208+spydersweb@users.noreply.github.com>
2019-07-15 21:39:07 +0100

    Initial commit

---
+++ EventLogger.vb
@@ -0,0 +1,43 @@
+﻿Imports System.Diagnostics
+
+<CLSCompliant(True)>
+Public Class EventLogger
+
+    Private _message As String
+
+    Public Property message()
+        Get
+            Return _message
+        End Get
+        Set(value)
+            _message &= value & vbCrLf
+        End Set
+    End Property
+
+    Public Sub New()
+
+    End Sub
+
+    Public Function WriteToEventLog(ByVal entry As String, _
+                                    Optional ByVal appName As String = "PHM", _
+                                    Optional ByVal eventType As EventLogEntryType = EventLogEntryType.Information, _
+                                    Optional ByVal logName As String = "Product name") As Boolean
+
+        Dim objectEventLog As New EventLog
+        Try
+            'Register the Application as an Event Source
+            If Not EventLog.SourceExists(appName) Then
+                EventLog.CreateEventSource(appName, logName)
+            End If
+
+            ' log the entry
+            objectEventLog.Source = appName
+            objectEventLog.WriteEntry(entry, eventType)
+            Return True
+
+        Catch ex As Exception
+            Return False
+
+        End Try
+    End Function
+End Class
